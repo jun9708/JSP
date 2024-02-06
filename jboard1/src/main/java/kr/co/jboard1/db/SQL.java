98e40f9a509dfda7edb5d637c7559f7201861645
wnsgud1997 <wnsgud1997@gmail.com>
2024-02-06 17:27:06 +0900

    JSP 게시판 실습

---
+++ jboard1/src/main/java/kr/co/jboard1/db/SQL.java
@@ -0,0 +1,29 @@
+package kr.co.jboard1.db;
+
+public class SQL {
+
+	
+	public static final String SELECT_TERMS = "SELECT * FROM `terms`";
+	
+	public static final String INSERT_USER = "INSERT INTO `User` SET "
+										   + "`uid`=?,"
+										   + "`pass`=SHA2(?, 256),"
+										   + "`name`=?,"
+										   + "`nick`=?,"
+										   + "`email`=?,"
+										   + "`hp`=?,"
+										   + "`regip`=?,"
+										   + "`rdate`=NOW()";
+	
+	public static final String SELECT_USER_FOR_LOGIN = "SELECT * FROM `User` WHERE `uid`=? AND `pass`=SHA2(?, 256)";
+	
+	
+	public static final String INSERT_ARTICLE = "INSERT INTO `Article` SET "
+												+ "`title`=?,"
+												+ "`content`=?,"
+												+ "`writer`=?,"
+												+ "`regip`=?,"
+												+ "`rdate`=NOW()";
+	
+	
+}
